20d45b6c900973b95cd575f57d40c2f985e06034
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-01-09 13:45:14 +0100

    build the project to the basic routing section

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php 
 require '../helpers.php';
-require basePath('views/home.view.php');
+
+// creating the basic routes
+$routes = [
+'/' => 'controllers/home.php',
+'/listings'=> 'controllers/listings/index.php',
+'/listings/create'=> 'controllers/listings/create.php',
+'404'=> 'controllers/error/404.php'
+];
+
+// getting the current url using the server super globals
+$uri = $_SERVER['REQUEST_URI'];  
+
+// getting the current url using the server super globals
+if(array_key_exists($uri, $routes)){
+require(basePath($routes[$uri]));
+}else{
+    require basePath($routes['404']);
+}
--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,7 +1,6 @@
 <?php 
 loadPartial('head');
 loadPartial('navbar');
-loadPartial('showcase');
 loadPartial('top-banner');
 ?>
 
@@ -189,10 +188,7 @@ loadPartial('top-banner');
             </div>
           </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-          <i class="fa fa-arrow-alt-circle-right"></i>
-          Show All Jobs
-        </a>
+      
       
     
     </section>
